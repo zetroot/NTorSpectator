45ce591904becdfffa729c0ea7767f9a3462dd3b
zetroot <zetroot@gmail.com>
2023-06-12 18:10:45 +0300

    Added migration

--- src/NTorSpectator.Database/DataContext.cs
+++ src/NTorSpectator.Database/DataContext.cs
@@ -5,6 +5,16 @@ namespace NTorSpectator.Database;
 
 public class DataContext : DbContext
 {
+    public DataContext()
+    {
+        
+    }
+    
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        
+    }
+
     public DbSet<Site> Sites => Set<Site>();
     public DbSet<Observation> Observations => Set<Observation>();
 
